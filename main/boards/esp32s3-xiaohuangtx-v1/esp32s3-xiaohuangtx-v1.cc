4c5e2bc464f3eed319bd1b2726c01f3b1bc79623
xiaohuangtx <hys578084@126.com>
2025-01-20 09:06:04 +0800

    Update chip scheme

--- main/boards/esp32s3-xiaohuangtx-v1/esp32s3-xiaohuangtx-v1.cc
+++ main/boards/esp32s3-xiaohuangtx-v1/esp32s3-xiaohuangtx-v1.cc
@@ -1,5 +1,5 @@
 #include "wifi_board.h"
-#include "audio_codecs/es8311_audio_codec.h"
+#include "audio_codecs/no_audio_codec.h"
 #include "display/ssd1306_display.h"
 #include "system_reset.h"
 #include "application.h"
@@ -16,7 +16,6 @@
 
 class Esp32s3XiaohuangtxV1 : public WifiBoard {
 private:
-    i2c_master_bus_handle_t codec_i2c_bus_;
     i2c_master_bus_handle_t display_i2c_bus_;
     Button boot_button_;
     Button touch_button_;
@@ -114,9 +113,13 @@ public:
     }
 
     virtual AudioCodec* GetAudioCodec() override {
-        static Es8311AudioCodec audio_codec(codec_i2c_bus_, I2C_NUM_0, AUDIO_INPUT_SAMPLE_RATE, AUDIO_OUTPUT_SAMPLE_RATE,
-            AUDIO_I2S_GPIO_MCLK, AUDIO_I2S_GPIO_BCLK, AUDIO_I2S_GPIO_WS, AUDIO_I2S_GPIO_DOUT, AUDIO_I2S_GPIO_DIN,
-            AUDIO_CODEC_PA_PIN, AUDIO_CODEC_ES8311_ADDR);
+#ifdef AUDIO_I2S_METHOD_SIMPLEX
+        static NoAudioCodecSimplex audio_codec(AUDIO_INPUT_SAMPLE_RATE, AUDIO_OUTPUT_SAMPLE_RATE,
+            AUDIO_I2S_SPK_GPIO_BCLK, AUDIO_I2S_SPK_GPIO_LRCK, AUDIO_I2S_SPK_GPIO_DOUT, AUDIO_I2S_MIC_GPIO_SCK, AUDIO_I2S_MIC_GPIO_WS, AUDIO_I2S_MIC_GPIO_DIN);
+#else
+        static NoAudioCodecDuplex audio_codec(AUDIO_INPUT_SAMPLE_RATE, AUDIO_OUTPUT_SAMPLE_RATE,
+            AUDIO_I2S_GPIO_BCLK, AUDIO_I2S_GPIO_WS, AUDIO_I2S_GPIO_DOUT, AUDIO_I2S_GPIO_DIN);
+#endif
         return &audio_codec;
     }
 
